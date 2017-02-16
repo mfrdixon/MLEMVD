5131992377d441591aa20372fb035852f1748aae
Matthew Dixon <matthewdixon@matthews-mbp-4.attlocal.net>
2017-02-16 08:54:47 -0600

    complete rewrite of Heston

--- drivers/Heston.R
+++ drivers/Heston.R
@@ -1,27 +1,39 @@
+source('inc.R')
+source('models/bivariate/ModelB6.R')
+source('drivers/getImpliedVolatility.R')
+source('models/bivariate/ModelHeston.R')
 
-source('logdensity2loglik.R')
-source('mymle.R')
-source('models/ModelB4.R')
-require(nloptr)
-require(pracma)
+require(Rcpp)
 
-args<-list(maxiter=50, eps=1e-8, print_level=3)
-eps <-1e-8
-#rho, kappa, theta, sigma
-args$l <- c(-1.0 + eps,eps,eps, eps)
-args$u <- c(1.0-eps,4.0-eps,1.0-eps,2.0-eps)
+sourceCpp('src/HestonFourierCosine.cpp')
+
+eps<-1e-3
 #rho, kappa, theta, sigma
 eval_g_ineq <- function (x) {
   grad <- c(0,-2.0*x[3],-2.0*x[2],2.0*x[4])
   return(list("constraints"=c(x[4]*x[4] - 2.0*x[2]*x[3]), "jacobian"=grad))  
 }
-# Model = B11;
-# dx1 = (k1 + k2*x2)*dt + sqrt(x2)*(sqrt(1 - rho^2)*dW1 + rho*dW2)
-# dx2 = kappa*(theta - x2)*dt + sigma*x2*dW2
-# 5 parameters to be estimated: (k1,k2,rho,kappa,theta,sigma)
+
+args<-list()
+args$N <- 128
+args$plot <- TRUE
+args$nloptr<-list(maxiter=100,
+                  method = 'NLOPT_LN_COBYLA',
+                  #rho, kappa, theta, sigma
+                  l = c(-1.0 + eps,eps,eps, eps),
+                  u = c(1.0-eps,4.0-eps,1.0-eps,2.0-eps),
+                  eval_g_ineq = eval_g_ineq,
+                  ftol_abs=1e-14, 
+                  xtol_rel=1e-14, 
+                  ftol_rel=1e-11, 
+                  print_level=3)
+
+args$DEoptim$maxiter    <- 10
+args$DEoptim$population <- 100
+args$DEoptim$strategy   <- 2
 
 # Heston model:
-# dln(S_t) = \mu dt + \sqrt{V_t}dW_t^{1}
+# dln(S_t) = (\mu - \sigma^2/2) dt + \sqrt{V_t}dW_t^{1}
 # dV_t = \kappa(\theta - V_t)dt  + \sigma \sqrt{V_t}dW_t^{2}
 # 4 parameters to be estimated: (rho, kappa, theta, sigma)  
   
@@ -32,9 +44,8 @@ S_0      <-50
 v_0      <-0.1  # the initial variance is assumed to be unknown
 rate     <-0.1  # the risk free rate is assumed to be known
 q        <-0
+mu_0     <-rate-q
 
-a_0      <- rate -q
-b_0      <- -0.5
 rho_0    <- -0.8
 kappa_0  <-3
 theta_0  <-0.2
@@ -68,9 +79,10 @@ x2simul[1] <- x2_0
 
 if(args$mode=='option'){
   T_0  <- 0.1
+  K_0  <- S_0
   callput <-'C'
   x3simul <- rep(0, nsimul)  
-  x3_0 <- HestonCOS(S_0,S_0,T_0,rate,q,sigma_0,kappa_0,theta_0,v_0,rho_0,'C')
+  x3_0 <- HestonCOS(S_0,K_0,T_0,rate,q,sigma_0,kappa_0,theta_0,v_0,rho_0,'C',args$N)
   x3simul[1] <- x3_0
 }
 
@@ -79,12 +91,13 @@ for (i in 2:nsimul){
   # dx2 = kappa*(theta - x2)*dt + sigma*sqrt(x2)*dW2
   x2simul[i] <- x2simul[i-1]+kappa_0*(theta_0 - x2simul[i-1])*delsimul + sigma_0*sqrt(x2simul[i-1])*sqrt(delsimul)*rndn2[i]
   # dx1 = (a_0 + b_0*x2)*dt + sqrt(x2)*(sqrt(1 - rho^2)*dW1 + rho*dW2)
-  x1simul[i] <- x1simul[i-1]+(a_0 + b_0*x2simul[i-1])*delsimul + sqrt(x2simul[i-1])*(sqrt(1 - rho_0^2)*sqrt(delsimul)*rndn1[i] + rho_0*sqrt(delsimul)*rndn2[i])    
+  x1simul[i] <- x1simul[i-1]+(mu_0*x2simul[i-1])*delsimul + sqrt(x2simul[i-1])*(sqrt(1 - rho_0^2)*sqrt(delsimul)*rndn1[i] + rho_0*sqrt(delsimul)*rndn2[i])    
   
   if(args$mode=='option'){ # calibrate to option prices
    S<-exp(x1simul[i])
    v<-x2simul[i]
-   x3simul[i] <- HestonCOS(S,S,T_0,rate,q,sigma_0,kappa_0,theta_0,v,rho_0,'C') 
+   K<-S
+   x3simul[i] <- HestonCOS(S,K,T_0,rate,q,sigma_0,kappa_0,theta_0,v,rho_0,'C',args$N) 
   }
 }
 
@@ -110,7 +123,11 @@ if (args$mode=='state'){
 }
 
 # change the parameter and determine whether the calibration can converge to the correct values
-param_0<-c(-0.3,2.0,0.1,0.2)
+#param_0<-c(-0.3,2.0,0.1,0.2)
 
 output <- mymle(ModelHeston, x, delta, param_0, args)
-print(output)
+res <- summary(output$solution,ModelHeston,x,delta,args)
+
+print(paste("Standard Error Estimate ", res$se))
+print(paste("Huber Sandwich Error Estimate ", res$se_robust))
+
--- drivers/getImpliedVolatility.R
+++ drivers/getImpliedVolatility.R
@@ -1,14 +1,23 @@
-getImpliedVolatility<-function(price, v_0, args){
+getImpliedVolatility<-function(price,K,T,param,v_0,args){
   
+  # rho   <- param[1]
+  # kappa <- param[2]
+  # theta <- param[3]
+  # sigma <- param[4]
   objfun<-function(v){
-    objfun<- abs(HestonCOS(S,K_0,T_0,rate,q,sigma,kappa,theta,v,rho,args$callput)-price) 
+    objfun<- abs(HestonCOS(S,K,T,rate,q,param[4],param[2],param[3],v,param[1],args$callput, args$N)-price)
+  }
+  jac<-function(v){
+    jac<-grad(objfun,v)
+      #HestonVega(S,K,T,rate,q,param[4],param[2],param[3],v,param[1],args$callput, args$N)
   }
   # Infer v_0 from observed option prices using a root finding method.
-  res<- nloptr( x0=v_0, 
-                eval_f=objfun, 
-                lb = 0.01, 
-                ub = 5, 
-                opts = list("algorithm"="NLOPT_LN_COBYLA", "maxeval" = 50, "xtol_rel" = args$eps, "print_level"=0))
+  res<- nloptr( x0=v_0,
+                eval_f=objfun,
+                eval_grad_f=jac,
+                lb = 0.1,
+                ub = 2.0,
+                opts = list("algorithm"='NLOPT_LD_LBFGS', "maxeval" = args$nloptr$maxiter, "xtol_rel" = args$nloptr$xtol_rel, "ftol_rel"= args$nloptr$ftol_rel, "ftol_abs"=args$nloptr$ftol_abs, "print_level"=0)) #args$nloptr$print_level
   
   return(res$solution)
 }
